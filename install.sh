b6a02a3ecca861879321d9fb72a6f41d5ae17631
cyberhost <103827425+Cyberhost-itservice@users.noreply.github.com>
2023-02-09 14:35:00 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+wget "https://download.passbolt.com/ce/installer/passbolt-repo-setup.ce.sh"
+wget "https://github.com/passbolt/passbolt-dep-scripts/releases/latest/download/passbolt-ce-SHA512SUM.txt"
+sha512sum -c passbolt-ce-SHA512SUM.txt && sudo bash ./passbolt-repo-setup.ce.sh || echo "Bad checksum. Aborting" && rm -f passbolt-repo-setup.ce.sh
+apt install passbolt-ce-server
