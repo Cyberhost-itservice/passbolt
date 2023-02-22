c46e4c05932b060c6d0e4d8388f15e5aa5c77e1f
cyberhost <103827425+Cyberhost-itservice@users.noreply.github.com>
2023-02-22 17:49:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ wget "https://download.passbolt.com/ce/installer/passbolt-repo-setup.ce.sh"
 wget "https://github.com/passbolt/passbolt-dep-scripts/releases/latest/download/passbolt-ce-SHA512SUM.txt"
 sha512sum -c passbolt-ce-SHA512SUM.txt && sudo bash ./passbolt-repo-setup.ce.sh || echo "Bad checksum. Aborting" && rm -f passbolt-repo-setup.ce.sh
 apt install passbolt-ce-server
+#https://www.howtoforge.com/install-and-configure-passbolt-password-manager-on-ubuntu-2004/ 
